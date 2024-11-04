18a7c901f0f04a928494039eca392c02443e3c23
Wasim Akram <mr.saim516@gmail.com>
2024-11-04 22:53:13 +0500

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mentalHealthQuiz.app',
+  appName: 'MHQ NU',
+  webDir: 'build'
+};
+
+export default config;
